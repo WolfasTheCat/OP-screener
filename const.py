f34df64ba2e56b6f515fe05f488a896e2bc58770
chodii <chodoraj1@gmail.com>
2024-05-14 10:23:33 +0200

    Update const.py

--- const.py
+++ const.py
@@ -5,6 +5,4 @@ Created on Fri Apr 26 13:38:35 2024
 @author: chodo
 """
 
-EDGAR_API_KEY   = "b4d964ddfa27806c1b5f94950cf55f03f9783d5aac6e69d1ef3670f648b0bc18"# reaching total limit
-
 RESULTING_FILE  ="screener.html"
