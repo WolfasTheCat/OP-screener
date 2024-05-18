824868be7588deeaf1fd6ce9aeeb16426c62387e
chodii <chodoraj1@gmail.com>
2024-05-18 23:30:09 +0200

    Color selection
    To support gray scale

--- screener.py
+++ screener.py
@@ -11,7 +11,6 @@ import plotly.express as plx
 import plotly.offline as plo
 
 import plotly.graph_objects as plgo
-import data_loader
 import plotly.subplots as plsub
 
 from datetime import datetime
@@ -30,14 +29,6 @@ def main():
     #    text_area = "<div>"+edgar_reports[k]["item_6"][:300]+"</div>"
     #    viz_objects["direct html"].append(text_area)
     
-    # As
-    """ EDGAR in pure HTML
-    edgar_reports = load_edgar(path="../edgar-crawler-main/datasets/RAW_FILINGS/", file_format=data_loader.Convertions.HTML)
-    edgar_reports.keys()
-    kys = edgar_reports['C:\\Users\\chodo\\Documents\\Studies\\Projects\\Masters\\KIV-VI\\semestrálka\\edgar-crawler-main\\datasets\\RAW_FILINGS\\320193_10K_2015_0001193125-15-356351.htm']
-    text_area = "<div>"+kys+"</div>"
-    viz_objects["direct html"].append(text_area)
-    """
     data = yf.download("^SPX", start="2018-01-01", end=datetime.now().strftime("%Y-%m-%d"))
     #viz_objects["visualization"].append(plx.bar(x=data.index, y=data["Close"]))
     
@@ -71,23 +62,25 @@ def hover_subplots(data, class_title, graph_title):
         
     )
     
-    plots = [
-        
-        plgo.Candlestick(x=data.index, open=data["Open"], high=data["High"], low=data["Low"], close=data["Close"], name="Candlestick", xaxis="x", yaxis="y"),
-        plgo.Line(x=data.index, y= data["Close"], name="Close", xaxis="x", yaxis="y", visible="legendonly")
-            ]
-    for sma_n in (10, 20, 30, 50):
+    plots = [plgo.Candlestick(x=data.index, open=data["Open"], high=data["High"], low=data["Low"], close=data["Close"], name="Price", xaxis="x", yaxis="y")]
+    #plots.append(plgo.Line(x=data.index, y= data["Close"], name="Close", xaxis="x", yaxis="y", visible="legendonly"))
+    
+    ma_range = (10, 20, 30, 50)
+    for sma_n in ma_range:
         close_SMA = indicators.SMA(data["Close"], sma_n)
         plots.append(
             plgo.Line(x=data.index[len(data["Close"])-len(close_SMA):], y= close_SMA, name="SMA("+str(sma_n)+")", xaxis="x", yaxis="y", visible=("legendonly" if sma_n!=20 else True)))
-    for ema_n in (10, 20, 30, 50):
+    
+    for ema_n in ma_range:
         close_EMA = indicators.EMA(data["Close"], ema_n)
         plots.append(
             plgo.Line(x=data.index[len(data["Close"])-len(close_EMA):], y= close_EMA, name="EMA("+str(ema_n)+")", xaxis="x", yaxis="y", visible="legendonly" if ema_n!=20 else True))
+    num_plots_1st = len(plots)
     for rsi_n in (9, 14, 26):
         close_RSI_n = indicators.RSI(data["Close"], rsi_n)
         plots.append(
             plgo.Line(x=data.index[len(data["Close"])-len(close_RSI_n):], y= close_RSI_n, name="RSI("+str(rsi_n)+")", xaxis="x", yaxis="y2", visible=("legendonly" if rsi_n!=14 else True)))
+    num_plots_2nd = len(plots) - num_plots_1st
     plots.append(
         plgo.Bar(x=data.index, y=data["Volume"], name="Volume", xaxis="x", yaxis="y3", marker=dict(color=data["Volume"], coloraxis="coloraxis")))
     
@@ -109,80 +102,22 @@ def hover_subplots(data, class_title, graph_title):
             ])
         )#, rangeslider_visible=True, rangeslider_thickness=0.15# default 0.15, rangeslider_bordercolor="black"
         )
+    # !! fixed indexes
+    fig.data[0].increasing.line.color = "rgba(0,239,0, 0.5)"
+    fig.data[0].increasing.fillcolor = "rgba(0,239,0, 0.5)"
+    
+    fig.data[0].decreasing.line.color = "rgba(127,127,0, 1)"
+    fig.data[0].decreasing.fillcolor = "rgba(127,127,0, 1)"
+    
+    clrs = ['#000000', '#D8D844', '#909000', '#A4A400', '#707000', '#505000', '#BFBF00', '#343400']
+    for i in range(1, num_plots_1st, 1):
+        fig['data'][i]['line']['color']=clrs[i-1]
+    for i in range(0, num_plots_2nd, 1):
+        fig['data'][num_plots_1st+i]['line']['color']=clrs[i]
     
     fig.update_yaxes(fixedrange=False)# for y and box zoom
     return fig
     
-def x_shared_subplots(data, class_title, graph_title):
-    fig = plsub.make_subplots(rows=3, cols=1,
-                        shared_xaxes=True,# = "columns", "rows", "all"
-                        vertical_spacing=0.01)
-    fig.update_layout(title_text=class_title+" for "+graph_title)
-    fig.update_legends(title_text=class_title)
-    
-    append_price_traces(data, fig, row=1, col=1)
-    
-    append_rsi_traces(data, fig, row=2, col=1)
-    
-    append_volume_traces(data, fig, row=3, col=1)
-    fig.update_xaxes(title_text="Time", row=3, col=1)
-    #fig.update_traces(marker=dict(color='#917A48'), row=3, col=1)
-    #fig.update_layout(coloraxis=dict(colorscale='Bluered'), showlegend=False)# ->Bluered_r for reversed scale
-    
-    #fig.update_xaxes(matches='x')# share axes
-    fig.update_layout(hovermode="x unified", coloraxis=dict(colorscale='solar_r', showscale=False, colorbar_orientation="h"))# show vertical line hover
-    #fig.update_xaxes(rangeslider_visible=True, row=3, col=1)# time-wheel
-    #, hovertemplate="%{y}%{_xother}"
-
-    fig.update_xaxes(# time buttons
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                #dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-    , row=1, col=1)
-    
-    fig.update_xaxes(# time range
-        rangeslider_visible=True,
-        
-        rangeslider_thickness=0.15# default 0.15
-        , row=3, col=1)
-    return fig
-
-def append_price_traces(data, fig, row, col):
-    fig.append_trace(#add_trace
-                     plgo.Line(x=data.index, y= data["Close"], name="Close")
-                , row=row, col=col)
-    for sma_n in (10, 20, 30, 50):
-        close_SMA = indicators.SMA(data["Close"], sma_n)
-        fig.append_trace(
-            plgo.Line(x=data.index[len(data["Close"])-len(close_SMA):], y= close_SMA, name="SMA("+str(sma_n)+")", visible="legendonly" if sma_n!=20 else True)
-        , row=row, col=col)
-    for ema_n in (10, 20, 30, 50):
-        close_EMA = indicators.EMA(data["Close"], ema_n)
-        fig.append_trace(
-            plgo.Line(x=data.index[len(data["Close"])-len(close_EMA):], y= close_EMA, name="EMA("+str(ema_n)+")", visible="legendonly" if ema_n!=20 else True)
-        , row=row, col=col)
-    fig.update_yaxes(title_text="Price [USD]", row=row, col=col)#, hovertemplate="%{y}%{_xother}"
-
-def append_rsi_traces(data, fig, row, col):
-    for rsi_n in (9, 14, 26):
-        close_RSI_n = indicators.RSI(data["Close"], rsi_n)
-        fig.append_trace(
-            plgo.Line(x=data.index[len(data["Close"])-len(close_RSI_n):], y= close_RSI_n, name="RSI("+str(rsi_n)+")", visible="legendonly" if rsi_n!=14 else True)
-    , row=row, col=col)
-    fig.update_yaxes(title_text="Relative Strength Index", row=row, col=col)
-
-def append_volume_traces(data, fig, row, col):
-    fig.append_trace(
-        plgo.Bar(x=data.index, y=data["Volume"], name="Volume", marker=dict(color=data["Volume"], coloraxis="coloraxis"))
-    , row=row, col=col)
-    fig.update_yaxes(title_text="Volume of Shares Traded", row=row, col=col)
-
 def price_graphs(data, title):
     #price_subs = moving_averages_subgraphs(data)
     
@@ -205,9 +140,26 @@ def PE_graph(company_ticker="MSFT"):
     fig.update_legends(title_text="Earnings Per Share Diluted [USD]")
     #fig = plx.scatter(x=PE_data["date"], y= PE_data["value"])
     return fig
-    
 
+"""
+def candlesticks_positive_negative(data):
+    data['change'] = data['Close'] - data['Open']
+    data_up = data[data['change']>0]
+    data_down = data[data['change']<=0]
+    cs_pl_go   = [plgo.Candlestick(x=data_up.index, open=data_up["Open"], high=data_up["High"], low=data_up["Low"], close=data_up["Close"], name="Positive Days", xaxis="x", yaxis="y"),
+               plgo.Candlestick(x=data_down.index, open=data_down["Open"], high=data_down["High"], low=data_down["Low"], close=data_down["Close"], name="Negative Days", xaxis="x", yaxis="y")]
+    return cs_pl_go
 
+fig.data[1].increasing.fillcolor = 'rgba(0,255,0, 255)'
+fig.data[1].increasing.line.color = 'rgba(0,127,0,255)'
+fig.data[1].decreasing.fillcolor = 'rgba(0,0,0,0)'
+fig.data[1].decreasing.line.color = 'rgba(0,0,0,0)'
+
+fig.data[2].increasing.fillcolor = 'rgba(0,0,0,0)'
+fig.data[2].increasing.line.color = 'rgba(0,0,0,0)'
+fig.data[2].decreasing.fillcolor = 'rgba(127,0,0,255)'
+fig.data[2].decreasing.line.color = 'rgba(255,0,0,255)'
+"""
 def multiple_graphs_on_page(viz_objects, file=const.RESULTING_FILE):
     with open(file,"w",encoding="utf8") as fig:# <3 \u2665
         fig.write("<html> <head> <title>Screener</title></head> ")
@@ -247,12 +199,5 @@ def multiple_graphs_on_page(viz_objects, file=const.RESULTING_FILE):
         
         fig.write("</body> </html>\n")
     
-
-#### CRAWLER ####
-def load_edgar(path, file_format=data_loader.Convertions.JSON_TO_DICT):
-    dator = data_loader.My_dataset(max_dataset_size=3)
-    dator.load_dataset(path = path, load_as = file_format)
-    return dator.dataset
-
 if __name__ == "__main__":
     main()
